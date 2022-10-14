9f8146a32b91c381b0c2bb24793a29320c5f0229
Anders Gustafsson <andersgee@gmail.com>
2022-10-14 16:35:39 +0200

    plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
+import PlausibleProvider from "next-plausible";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <PlausibleProvider domain="cssbattlesinsvg.andyfx.net">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 };
 
 export default MyApp;
